98831da289a7e9bc2faabac8fea542faff19d29d
Adrien Franto <radrienfrantos@gmail.com>
2025-08-16 16:29:21 +0200

    2 – Ajouter une méthode de test à la classe MoneyTest

--- src/test/java/fr/emse/test/MoneyTest.java
+++ src/test/java/fr/emse/test/MoneyTest.java
@@ -6,9 +6,13 @@ import org.junit.Test;
 
 public class MoneyTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test 
+	public void testSimpleAdd() { 
+		Money m12CHF = new Money(12, "CHF"); 
+		Money m14CHF = new Money(14, "CHF"); 
+		Money expected = new Money(26, "CHF"); 
+		Money result = m12CHF.add(m14CHF);  
+		assertTrue(expected.equals(result));
 	}
 
 }
