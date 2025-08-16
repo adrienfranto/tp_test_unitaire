27200368dcf370485d6c603833160f61dc37f383
Adrien Franto <radrienfrantos@gmail.com>
2025-08-16 17:57:44 +0200

    7 – Refactor MoneyTest: use @Before to remove duplication

--- src/test/java/fr/emse/test/MoneyTest.java
+++ src/test/java/fr/emse/test/MoneyTest.java
@@ -3,14 +3,22 @@ package fr.emse.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
+	
+	private Money m12CHF;
+    private Money m14CHF;
+
+    @Before
+    public void setUp() {
+        m12CHF = new Money(12, "CHF");
+        m14CHF = new Money(14, "CHF");
+    }
 
 	@Test 
-	public void testSimpleAdd() { 
-		Money m12CHF = new Money(12, "CHF"); 
-		Money m14CHF = new Money(14, "CHF"); 
+	public void testSimpleAdd() {  
 		Money expected = new Money(26, "CHF"); 
 		Money result = m12CHF.add(m14CHF);  
 		assertTrue(expected.equals(result));
@@ -18,8 +26,6 @@ public class MoneyTest {
 	
 	@Test 
 	public void testEquals() { 
-		Money m12CHF= new Money(12, "CHF"); 
-		Money m14CHF= new Money(14, "CHF"); 
 		assertTrue(!m12CHF.equals(null)); 
 		assertEquals(m12CHF, m12CHF); 
 		assertEquals(m12CHF, new Money(12, "CHF")); 
